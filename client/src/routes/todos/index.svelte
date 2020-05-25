6de1da077c6836413824fca88ac757e94cd3cb40
Frank West <frank.west.iii@gmail.com>
2020-05-25 10:45:35 -0700

    Simple todo listing

---
+++ client/src/routes/todos/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let todos = [];
+
+  onMount(async () => {
+    const res = await fetch('http://localhost:3000/todos');
+    todos = await res.json();
+  });
+</script>
+
+{#each todos as todo}
+  <p>{todo.title}</p>
+{/each}
