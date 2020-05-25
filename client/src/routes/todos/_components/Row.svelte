15d05af47c61c7081b42759f49dc1ab12cf3453d
Frank West <frank.west.iii@gmail.com>
2020-05-25 13:31:41 -0700

    Add tailwind

--- client/src/routes/todos/_components/Row.svelte
+++ client/src/routes/todos/_components/Row.svelte
@@ -1,18 +1,19 @@
 <script>
 	export let todo;
 
-  const handleChange = () =>
-    fetch(`http://localhost:3000/todos/${todo.id}`, {
-      method: 'PUT',
-      body: JSON.stringify({ todo }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+	const handleChange = () =>
+		fetch(`http://localhost:3000/todos/${todo.id}`, {
+			method: 'PUT',
+			body: JSON.stringify({ todo }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
 </script>
 
-
-  <div>
-    <a rel=prefetch href="/todos/{todo.id}">{todo.title}</a>
-    <input type="checkbox" bind:checked={todo.completed} on:change={handleChange} />
-  </div>
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+	<div class="px-6 py-4">
+		<a rel=prefetch href="/todos/{todo.id}">{todo.title}</a>
+		<input type="checkbox" bind:checked={todo.completed} on:change={handleChange} />
+	</div>
+</div>
